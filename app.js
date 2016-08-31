ae6229b4fa842f8962423ad18a13f5a390af52e1
Rakin Uddin <rakinuddin97@gmail.com>
2016-08-31 11:48:45 -0400

    added list of posts to app.js; displayed contents in index.html using ng-repeat with ordering

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 var app = angular.module('newsApp', []);
 
-app.controller('MainController', ['$scope', 
-	function($scope){$scope.test = 'Hello World'}]);
+app.controller('MainController', 
+	['$scope', 
+	function($scope){
+		$scope.test = 'Hello World'
+		$scope.posts = [
+						{title: 'post 1', upvotes: 5},
+						{title: 'post 2', upvotes: 2},
+						{title: 'post 3', upvotes: 6},
+						{title: 'post 4', upvotes: 10},
+						{title: 'post 5', upvotes: 21}
+						];
+					}
+	]);
