3f3ed2cbd998d8bb5b948ded45ae6f6c31d071bb
Rakin Uddin <rakinuddin97@gmail.com>
2016-08-31 12:25:22 -0400

    added scope function which increments the upvotes var by 1

--- app.js
+++ app.js
@@ -16,5 +16,8 @@ app.controller('MainController',
 			$scope.posts.push({title: $scope.title, upvotes: 0});
 			$scope.title='';
 		};
+		$scope.incrementUpvotes = function(post) {
+			post.upvotes += 1;
+		};
 					}
 	]);
