6ebbc3f00ded0627686f4f73444ca60100a78635
Rakin Uddin <rakinuddin97@gmail.com>
2016-09-02 00:00:07 -0400

    Added documentation; Formatted code for more aestheticity

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
+// setting up the module
 var app = angular.module('newsApp', []);
 
+// creating a new controller
 app.controller('MainController', 
 	['$scope', 
 	function($scope){
-		$scope.test = 'Hello World'
+		// list of posts with title and upvotes elements
 		$scope.posts = [
-						{title: 'post 1', upvotes: 5},
-						{title: 'post 2', upvotes: 2},
-						{title: 'post 3', upvotes: 6},
-						{title: 'post 4', upvotes: 10},
-						{title: 'post 5', upvotes: 21}
-						];
+			{title: 'post 1', upvotes: 5},
+			{title: 'post 2', upvotes: 2},
+			{title: 'post 3', upvotes: 6},
+			{title: 'post 4', upvotes: 10},
+			{title: 'post 5', upvotes: 21}
+			];
+
 		$scope.addPost = function(){
+			// add a post iff the title exists and is non-empty
 			if(!$scope.title || $scope.title === '') { return; }
 			$scope.posts.push({
 				title: $scope.title,
@@ -20,8 +24,10 @@ app.controller('MainController',
 			$scope.title='';
 			$scope.link='';
 		};
+
+		// function to increase upvotes; requires a post element
 		$scope.incrementUpvotes = function(post) {
 			post.upvotes += 1;
 		};
-					}
+	}
 	]);
