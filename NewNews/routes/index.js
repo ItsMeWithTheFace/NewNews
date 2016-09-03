83e315eb129c428b2646daab3aab543f53120463
Rakin Uddin <rakinuddin97@gmail.com>
2016-09-03 17:34:45 -0400

    added routes for preloading and returning post objects

--- NewNews/routes/index.js
+++ NewNews/routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var router = express.Router();
+
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
-var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,6 +19,7 @@ router.get('/posts', function(req, res, next) {
 	});
 });
 
+/* POST comments onto posts page */
 router.post('/posts', function(req, res, next) {
 	var post = new Post(req.body);
 
@@ -27,5 +29,24 @@ router.post('/posts', function(req, res, next) {
 		res.json(post);
 	});
 });
+
+/* route to preload post objects */
+router.param('post', function(req, res, next, id) {
+	var query = Post.findById(id);
+
+	query.exec(function(err, post) {
+		if(err){ return next(err); }
+		if(!post) { return next(new Error('can\'t find post')); }
+
+		req.post = post;
+		return next();
+	});
+});
+
+/* route for returning a single post */
+router.get('/posts/:post', function(req, res) {
+	res.json(req.post);
+});
+
 
 module.exports = router;
