79cacdfdffed3d15c3b14fbc18c5e8fcd18d01e9
Rakin Uddin <rakinuddin97@gmail.com>
2016-09-05 11:59:16 -0400

    modified GET /post/:post route to populate each post with its own comments

--- NewNews/routes/index.js
+++ NewNews/routes/index.js
@@ -45,7 +45,11 @@ router.param('post', function(req, res, next, id) {
 
 /* route for returning a single post */
 router.get('/posts/:post', function(req, res) {
-	res.json(req.post);
+	req.post.populate('comments', function(err, post) {	// retrieve comments along with posts
+		if(err) { return next(err); }
+
+		res.json(req.post);
+	});
 });
 
 /* upvoting posts route */
@@ -57,5 +61,44 @@ router.put('/posts/:post/upvote', function(req, res, next) {
 	});
 });
 
+/* route for comments on a particular post */
+router.post('/posts/:post/comments', function(req, res, next) {
+	var comment = new Comment(req.body);
+	comment.post = req.post;
+
+	comment.save(function(err, comment){
+		if(err){ return next(err); }
+
+		req.post.comments.push(comment);
+		req.post.save(function(err, post){
+			if(err) { return next(err); }
+
+			res.json(comment);
+		});
+	});
+});
+
+/* upvotes of the comments */
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+	req.post.comment.upvote(function(err, post){
+		if(err) { return next(err); }
+
+		res.json(post);
+	});
+});
+
+/* middleware function to retrieve comments specified by
+the :comment route parameter */
+router.param('/posts/:post/comments/:comment', function(req, res, next, id) {
+	var query = Comment.findById(id);
+
+	query.exec(function(err, comment) {
+		if(err) { return next(err); }
+		if(!comment) { return next(new Error('can\'t find comment')); }
+
+		req.comment = comment;
+		return next();
+	})
+})
 
 module.exports = router;
