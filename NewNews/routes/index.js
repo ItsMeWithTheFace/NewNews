b813e24f52b6ea74ff37a8d6d6e3794e286928aa
Rakin Uddin <rakinuddin97@gmail.com>
2016-09-18 14:26:09 -0400

    added routes to index.js to register/authenticate users. updated previously made routes to require authentication for commenting, posting and upvoting

--- NewNews/routes/index.js
+++ NewNews/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var router = express.Router();
+var passport = require('passport');
+var jwt = require('express-jwt');
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,8 +24,9 @@ router.get('/posts', function(req, res, next) {
 });
 
 /* POST comments onto posts page */
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
 	var post = new Post(req.body);
+	post.author = req.payload.username;
 
 	post.save(function(err, post){
 		if(err){ return next(err); }
@@ -53,7 +58,7 @@ router.get('/posts/:post', function(req, res) {
 });
 
 /* upvoting posts route */
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
 	req.post.upvote(function(err, post) {
 		if(err) { return next(err); }
 
@@ -62,9 +67,10 @@ router.put('/posts/:post/upvote', function(req, res, next) {
 });
 
 /* route for comments on a particular post */
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
 	var comment = new Comment(req.body);
 	comment.post = req.post;
+	comment.author = req.payload.username;
 
 	comment.save(function(err, comment){
 		if(err){ return next(err); }
@@ -79,7 +85,7 @@ router.post('/posts/:post/comments', function(req, res, next) {
 });
 
 /* upvotes of the comments */
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
 	req.post.comment.upvote(function(err, post){
 		if(err) { return next(err); }
 
@@ -100,5 +106,41 @@ router.param('comment', function(req, res, next, id) {
 		return next();
 	})
 })
+
+// route to create a user with a given username and password
+router.post('/register', function(req, res, next){
+	if(!req.body.username || !req.body.password){
+		return res.status(400).json({message: 'Please fill out all fields'});
+	}
+
+	var user = new User();
+
+	user.username = req.body.username;
+
+	user.setPasswod(req.body.password)
+
+	user.save(function (err){
+		if(err){ return next(err); }
+
+		return res.json({token: user.generateJWT()})
+	}) ;
+});
+
+// authenticates a user and returns token to client
+router.post('/login', function(req, res, next){
+	if(!req.body.username || !req.body.password){
+		return res.status(400).json({message: 'Please fill out all fields'});
+	}
+
+	passport.authenticate('local', function(err, user, info){
+		if(err){ return next(err); }
+
+		if(user){
+			return res.json({token: user.generateJWT()});
+		} else {
+			return res.status(401).json(info);
+		}
+	})(req, res, next);
+});
 
 module.exports = router;
